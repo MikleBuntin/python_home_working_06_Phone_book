de340493155dc3749ecda8cd201c6154e17d858e
MikleBuntin <BuntinMA@mail.ru>
2022-11-02 19:57:18 +0300

    Закодил html saver

--- logger.py
+++ logger.py
@@ -27,8 +27,16 @@ def save_to_csv(f_name):
     with open(f_name + ".csv", 'w') as new_file:
         new_file.write(phone_book)
         print("Данные сохранены в файл {}.csv.".format(f_name))
-#
-# def save_to_html(f_name):
-#
 
+def save_to_html(f_name):
+    with open('phone_book.csv', 'r') as file:
+        style = 'style="font-size:30px;"'
+        phone_book = '<html>\n  <head></head>\n  <body>\n'
+        for line in file:
+            line = line.split(';')
+            phone_book += '    <p {}>Name: {}, phone: {} ({}) </p>\n' .format(style, line[0], line[1], line[2])
+        phone_book += '  </body>\n</html>'
 
+    with open(f_name + ".html", 'w') as new_file:
+        new_file.write(phone_book)
+        print("Данные сохранены в файл {}.html.".format(f_name))
